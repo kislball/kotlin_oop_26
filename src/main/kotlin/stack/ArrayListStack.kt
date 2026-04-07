ecc75d5e573c853f0d370f00fa0fbfd68f80cbe4
Georgiy Mikhailenko <kislball@yahoo.com>
2026-04-07 15:44:08 +0300

    Implement ArrayListStack

--- src/main/kotlin/stack/ArrayListStack.kt
+++ src/main/kotlin/stack/ArrayListStack.kt
@@ -1,5 +1,41 @@
 package org.example.stack
 
-class ArrayListStack : Stack {
-    // TODO: use delegation instead of inheritance here, use CustomArrayList
+import org.example.list.CustomArrayList
+
+class ArrayListStack(val inner: CustomArrayList) : Stack {
+  override fun push(value: Int) {
+    addFirst(value)
+  }
+
+  override fun pop(): Int {
+    val v = peek()
+    remove(0)
+
+    return v
+  }
+
+  override fun peek(): Int {
+    return get(0)
+  }
+
+  override val isEmpty: Boolean
+    get() = size == 0
+
+  override fun get(index: Int): Int = inner.get(index)
+
+  override fun set(index: Int, value: Int) = inner.set(index, value)
+
+  override fun add(element: Int) = inner.add(element)
+
+  override fun addFirst(element: Int) = inner.addFirst(element)
+
+  override fun remove(element: Int): Boolean = inner.remove(element)
+
+  override fun indexOf(element: Int): Int = inner.indexOf(element)
+
+  override val size: Int
+    get() = inner.size
+
+  override fun iterator(): Iterator<Int> = inner.iterator()
 }
+
